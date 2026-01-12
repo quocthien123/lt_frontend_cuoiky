01bcac1a22dea3116e1820b60281ea4ab26ae570
quocthien123 <22130263@st.hcmuaf.edu.vn>
2026-01-12 09:38:12 +0700

    backend-video - báo nói - chế độ sáng tối

--- frontend_backend_news/.next/dev/types/validator.ts
+++ frontend_backend_news/.next/dev/types/validator.ts
@@ -54,6 +54,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../src/app/api/videos/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/videos">> = Specific
+  const handler = {} as typeof import("../../../src/app/api/videos/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
