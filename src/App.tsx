ad0d751f53ff0814d8f87ad72ac0bd4426facf68
Vo Tan Sang <22130233@st.hcmuaf.edu.vn>
2026-01-13 23:55:20 +0700

    cập nhật lại App.tsx dùng AuthProvider bọc ngoài Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import HomePage from './pages/HomePage';
 import CategoryPage from './pages/CategoryPage';
 import NewPage from './pages/News/NewsPage'
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <HomePage />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/danh-muc/:slug"
-          element={
-            <CategoryPage/>
-          }
+import {RegisterPage} from "@/pages/Auth/Register/RegisterPage.tsx";
+import {LoginPage} from "@/pages/Auth/Login/LoginPage.tsx";
+import {AuthProvider} from "@/context/AuthContext.tsx";
 
-          
-        />
-        <Route path="/:category/:slug" element={<NewPage />} />
-        
-        {/* Additional routes: /matches, /news, /standings... will be added later */}
-      </Routes>
-    </Router>
-  );
+export default function App() {
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <MainLayout>
+                                <HomePage/>
+                            </MainLayout>
+                        }
+                    />
+                    <Route path="/register" element={<RegisterPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+
+                    <Route
+                        path="/danh-muc/:slug"
+                        element={
+                            <CategoryPage/>
+                        }
+
+
+                    />
+                    <Route path="/:category/:slug" element={<NewPage/>}/>
+
+                    {/* Additional routes: /matches, /news, /standings... will be added later */}
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
